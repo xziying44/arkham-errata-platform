9d168a35a30933ddcc612ac606f031bccaa7fa6e
xziying <xziying@vip.qq.com>
2026-04-26 17:13:49 +0800

    修正取消勘误迁移版本号

--- backend/alembic/versions/20260426_cancel_action.py
+++ backend/alembic/versions/20260426_cancel_action.py
@@ -1,6 +1,6 @@
 """添加取消勘误审计动作
 
-Revision ID: 20260426_add_cancel_errata_action
+Revision ID: 20260426_cancel_action
 Revises: 20260426_errata_workbench
 Create Date: 2026-04-26 16:30:00.000000
 """
@@ -10,7 +10,7 @@ from typing import Sequence, Union
 from alembic import op
 
 
-revision: str = "20260426_add_cancel_errata_action"
+revision: str = "20260426_cancel_action"
 down_revision: Union[str, None] = "20260426_errata_workbench"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
